dd25d95e1b0db9fd955d2c040b8827acbd2bf4c6
Dima Kagan <dimakagan15@gmail.com>
2017-12-24 18:18:26 +0200

    Examples added

---
+++ examples/cross_val_example.py
@@ -0,0 +1,10 @@
+from turicreate_cross_validation.cross_validation import *
+
+if __name__ == "__main__":
+    url = 'https://static.turi.com/datasets/xgboost/mushroom.csv'
+    sf = tc.SFrame.read_csv(url)
+    sf['label'] = (sf['label'] == 'p')
+    params = {'target': 'label'}
+    sf = shuffle_sframe(sf)
+    folds = StratifiedKFold(sf, 'label', 5)
+    cross_val_score(folds, tc.random_forest_classifier.create, params, label='label')
