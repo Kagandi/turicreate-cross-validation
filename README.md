70b16a1edc9a95d83357a1a38739aaeea772f09e
Dima Kagan <dimakagan15@gmail.com>
2021-02-17 17:21:01 +0200

    Add verbose:False to the example.

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ from turicreate_cross_validation import shuffle_sframe, StratifiedKFold, cross_v
 url = 'https://static.turi.com/datasets/xgboost/mushroom.csv'
 sf = tc.SFrame.read_csv(url)
 sf['label'] = (sf['label'] == 'p')
-params = {'target': 'label'}
+params = {'target': 'label', "verbose": False}
 sf = shuffle_sframe(sf)
 folds = StratifiedKFold(sf, 'label', 5)
 print(cross_val_score(folds, tc.random_forest_classifier.create, params))
--- examples/cross_val_example.py
+++ examples/cross_val_example.py
@@ -1,11 +1,11 @@
 import turicreate as tc
-from turicreate_cross_validation.cross_validation import shuffle_sframe, StratifiedKFold, cross_val_score
+from turicreate_cross_validation import shuffle_sframe, StratifiedKFold, cross_val_score
 
 if __name__ == "__main__":
     url = 'https://static.turi.com/datasets/xgboost/mushroom.csv'
     sf = tc.SFrame.read_csv(url)
     sf['label'] = (sf['label'] == 'p')
-    params = {'target': 'label'}
+    params = {'target': 'label', "verbose": False}
     sf = shuffle_sframe(sf)
     folds = StratifiedKFold(sf, 'label', 5)
     print(cross_val_score(folds, tc.random_forest_classifier.create, params))
